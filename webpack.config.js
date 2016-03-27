10884bd2597a450f3c7e4409d1aaa1268202158b
miyamoriaoi <miyamori@126.com>
2016-03-27 19:25:49 +0800

    react development ok

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+var config = {
+    entry: [path.resolve(__dirname, 'app/js/main.js')],
+    output: {
+        path: path.resolve(__dirname, './dist/'),
+        filename: 'space.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                  presets: ['es2015', 'react']
+                }
+            }
+        ],
+        resolve: {
+            extensions:['','.js','.jsx']
+        },
+    }
+};
+
+module.exports = config;
